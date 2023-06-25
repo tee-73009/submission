f4c0feb88c00971e1911d82734f0f03ee241ae21
tee-73009 <136820692+tee-73009@users.noreply.github.com>
2023-06-25 13:33:52 +0000

    [APG4b] APG4b_cs

---
+++ APG4b/APG4b_cs/APG4b_cs.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // 一年の秒数
+  int a = 365 * 24 * 60 * 60;
+
+  // 以下のコメント/* */を消して追記する
+  cout << a << endl;
+  cout << 2*a << endl;
+  cout << 5*a << endl;
+  cout << 10*a << endl;
+}
