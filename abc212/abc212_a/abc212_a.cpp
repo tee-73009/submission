1053faa5bafb78b34b58ba62ec5ddb11040e46ea
tee-73009 <136820692+tee-73009@users.noreply.github.com>
2023-07-22 07:08:28 +0000

    [abc212] abc212_a

---
+++ abc212/abc212_a/abc212_a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a,b;
+  cin>>a>>b;
+  if(a==0)cout << "Silver";
+  else if(b==0) cout << "Gold";
+  else cout << "Alloy";
+  cout << endl;
+}
