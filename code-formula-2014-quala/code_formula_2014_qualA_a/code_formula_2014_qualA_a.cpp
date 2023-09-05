8962d89129d07121dd31e54267350abf942592c9
tee-73009 <136820692+tee-73009@users.noreply.github.com>
2023-09-05 08:54:05 +0000

    [code-formula-2014-quala] code_formula_2014_qualA_a

---
+++ code-formula-2014-quala/code_formula_2014_qualA_a/code_formula_2014_qualA_a.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define llrep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rrep(i, a, b) for(int i = a; i < (int)(b); i++)
+#define llrrep(i, a, b) for(ll i = a; i < (ll)(b); i++)
+#define all(x) (x).begin,(x).end()
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+#define YESNO(T) if(T){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define YesNo(T) if(T){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+using namespace std;
+using ll=long long;
+using ld=long double;
+using ull=unsigned long long;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+using Graph = vector<vector<ll>>;
+const string ABC="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string abc="abcdefghijklmnopqrstuvwxyz";
+#define gcd __gcd
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define print(n) cout << n << endl
+template <typename T>
+inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template <typename T>
+inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+const ll mod=998244353;
+const ll MOD=1000000007;
+int main() {
+  ll a;
+  cin >> a;
+  int ans = 0;
+  for(int i=0;i*i*i<=a;i++) if(i*i*i==a) ans++;
+  YESNO(ans>0);
+}
