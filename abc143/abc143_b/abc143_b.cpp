26b27970a1338af7f97bf8877a6d1665228dd24e
tee-73009 <136820692+tee-73009@users.noreply.github.com>
2023-12-31 02:10:49 +0000

    [abc143] abc143_b

--- abc143/abc143_b/abc143_b.cpp
+++ abc143/abc143_b/abc143_b.cpp
@@ -530,12 +530,12 @@ int digsum(int n) {
     return res;
 }
 //素数判定
-ull isPrime(ull x) {
+ll isPrime(ll x) {
     ll i;
     if(x < 2)return 0;
     else if(x == 2) return 1;
     if(x%2 == 0) return 0;
-    for(i = 3; i*i <= x; i += 2) if(x%i == 0) return i;
+    for(i = 3; i*i <= x; i += 2) if(x%i == 0) return 0;
     return 1;
 }
 //エラトステネスの篩（素数取得）
